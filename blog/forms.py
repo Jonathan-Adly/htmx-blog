dabb36b5dc372548001c6e1f01bf912f1e7dc75d
Jonathan Adly <jonathanadly@jonathans-air.myfiosgateway.com>
2021-10-31 08:09:31 -0400

    reverse_lazy

--- blog/forms.py
+++ blog/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from .models import Blog
+from django.urls import reverse_lazy
 
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ("title", "content")
-
+        link = reverse_lazy("preview")
         widgets = {
             "content": forms.Textarea(
                 attrs={
                     "class": "h-400",
-                    "hx-post": "/preview",
+                    "hx-post": link,
                     "hx-trigger": "keyup changed delay:500ms",
                     "hx-target": "#preview",
                 }
